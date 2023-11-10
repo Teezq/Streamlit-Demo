a24c8187c81009ef532f90bb32b7c6a7739bcdf2
Teezq <87978642+Teezq@users.noreply.github.com>
2023-11-10 13:30:06 +0800

    Upload main.py

---
+++ data/main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.write("Hi :smile:")
+
+# streamlit
+# run
+# C:\Users\ZhenQuanTee\anaconda3\lib\site - packages\ipykernel_launcher.py
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
